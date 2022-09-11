e891b7d500c715a1d56d1ed3d6fbbba0b07f0e42
PurdyRad <adampurdy407@gmail.com>
2022-09-10 20:37:44 -0400

    introduced props/filler data

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Todo = ({theTask}) => {
   return (
     <div>
-        <div> Let's do this: {theTask.todo_text} </div>
+        <div> Let's do this: {theTask.todo_text}! </div>
+        
     </div>
   )
 }
