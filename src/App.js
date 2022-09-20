8bccf048a152c148a460090d7374e6e28e89c4d0
PurdyRad <adampurdy407@gmail.com>
2022-09-20 13:23:27 -0400

    updated local state for todos

--- src/App.js
+++ src/App.js
@@ -6,42 +6,64 @@ const dummyData = [
   {
     id: 1,
     text: "Go workout",
-    todo_completed: false
+    completed: false
   },
   {
     id: 2,
     text: "Read",
-    todo_completed: false
+    completed: false
   },
   {
     id: 3,
     text: "Floss",
-    todo_completed: false
+    completed: false
   },
   {
     id: 4,
     text: "Eat some yummy food",
-    todo_completed: false
+    completed: false
   },
 ]
 
+const LOCAL_STORAGE_KEY = 'react-todo-list-todos';
 
 function App() {
   const [todoData, setTodoData] = useState(dummyData);
+  
+  useEffect(() => {
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storageTodos) {
+      setTodoData(storageTodos)
+    }
+  }, []);
 
   useEffect(() => {
-    localStorage.setItem('react-todo-list-todos', JSON.stringify(todoData));
-  }, [todoData])
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todoData));
+  }, [todoData]);
+
 
   const addTodo = todo => {
     setTodoData([todo, ...todoData])
+  }
+
+  const toggleComplete = (id) => {
+    setTodoData(
+      todoData.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo, completed: todo.completed
+          };
+        }
+        return todo;
+      })
+    );
   }
 
   return (
     <div className="App" style = {{ backgroundColor: 'grey' }}>
       <h1>What should we get done?</h1>
       <TodoForm addTodo={addTodo}/>
-      <TodoList stuffTodo={todoData} />
+      <TodoList stuffTodo={todoData} toggleComplete={toggleComplete} />
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Todo from './Todo'
 
-const TodoList = ({stuffTodo}) => {
+const TodoList = ({stuffTodo, toggleComplete}) => {
 
   return (
     <div>
         <h4>Todo List:</h4>
         <ul>
         {stuffTodo.map((task) =>  (
-             <Todo key={task.id} theTask={task} /> )) }
+             <Todo key={task.id} theTask={task} toggleComplete={toggleComplete} /> )) }
         </ul>
 
     </div>
